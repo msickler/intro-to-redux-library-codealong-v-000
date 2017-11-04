1db5c7bae33ac9b3f094046785160989d6c6d0a6
Mariana Sickler <marianaleal_93@hotmail.com>
2017-11-04 13:02:39 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 import shoppingListItemReducer from './reducers/shoppingListItemReducer';
 import App from './App';
 import './index.css';
+
+const store = createStore(shoppingListItemReducer);
 
 ReactDOM.render(
   <App />,
